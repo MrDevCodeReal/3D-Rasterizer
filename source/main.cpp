8e0e3eba86e54320a120fd50dd9ad9002b757062
MrDevCodeReal <143895393+MrDevCodeReal@users.noreply.github.com>
2026-03-22 03:17:04 +0300

    fixed a drawing bug that was linked to the clear color buffer

--- source/main.cpp
+++ source/main.cpp
@@ -12,10 +12,10 @@ bool pointOnRight(Vector2 a, Vector2 b, Vector2 c){
     return (Vector2DotProduct(AC, ABPERP) >= 0);
 }
 
-bool pointInTriangle(Vector2 point, Vector2 triangle[]){
-    bool A = pointOnRight(triangle[0], triangle[1], point);
-    bool B = pointOnRight(triangle[1], triangle[2], point);
-    bool C = pointOnRight(triangle[2], triangle[0], point);
+bool pointInTriangle(Vector2 point, Vector2 triangle[], int indices[]){
+    bool A = pointOnRight(triangle[indices[0]], triangle[indices[1]], point);
+    bool B = pointOnRight(triangle[indices[1]], triangle[indices[2]], point);
+    bool C = pointOnRight(triangle[indices[2]], triangle[indices[0]], point);
 
     return (A && B && C);
 }
@@ -32,42 +32,43 @@ int main(){
     Color colorBuffer[480][640] = {};
 
     //triangle info
-    Vector2 triangle_points[3] = {{213, 320}, {426, 320}, {320, 160}};
+    Vector2 triangle_points[3] = {{0, 0},  {0, 480}, {640, 480}};
+    int triangle_indices[3] = {0, 1, 2};
 
     //bounding box to ensure speed
-    int bounding_box[4] = {9999, -9999, 9999, -9999};
+    int bounding_box[4];
 
     Image img = GenImageColor(screenWidth, screenHeight, BLACK);
     Texture2D tex = LoadTextureFromImage(img);
 
 	while (!WindowShouldClose()){
 
-        bounding_box[0] = 9999;
-        bounding_box[1] = -9999;
-        bounding_box[2] = 9999;
-        bounding_box[3] = -9999;
+        bounding_box[0] = 999;
+        bounding_box[1] = -999;
+        bounding_box[2] = 999;
+        bounding_box[3] = -999;
         for (int i=0; i<3; i++){
-            if (triangle_points[i].y < bounding_box[0]){
-                bounding_box[0] = triangle_points[i].y;
+            if (triangle_points[triangle_indices[i]].y < bounding_box[0]){
+                bounding_box[0] = triangle_points[triangle_indices[i]].y;
             }
-            if (triangle_points[i].y > bounding_box[1]){
-                bounding_box[1] = triangle_points[i].y;
+            if (triangle_points[triangle_indices[i]].y > bounding_box[1]){
+                bounding_box[1] = triangle_points[triangle_indices[i]].y;
             }
-            if (triangle_points[i].x < bounding_box[2]){
-                bounding_box[2] = triangle_points[i].x;
+            if (triangle_points[triangle_indices[i]].x < bounding_box[2]){
+                bounding_box[2] = triangle_points[triangle_indices[i]].x;
             }
-            if (triangle_points[i].x > bounding_box[3]){
-                bounding_box[3] = triangle_points[i].x;
+            if (triangle_points[triangle_indices[i]].x > bounding_box[3]){
+                bounding_box[3] = triangle_points[triangle_indices[i]].x;
             }
         }
 
-        for (int y=0; y<screenHeight; y++)
-            for (int x=0; x<screenWidth; x++)
-                colorBuffer[y][x] = {25, 25, 45};
+        for (int y=0; y<=screenHeight-1; y++)
+            for (int x=0; x<=screenWidth-1; x++)
+                colorBuffer[y][x] = {255, 255, 255};
 
-        for (int py=bounding_box[0]; py<=bounding_box[1]; py++){
-            for (int px=bounding_box[2]; px<=bounding_box[3]; px++){
-                if (pointInTriangle({(float)px, (float)py}, triangle_points)){
+        for (int py=bounding_box[0]; py<bounding_box[1]; py++){
+            for (int px=bounding_box[2]; px<bounding_box[3]; px++){
+                if (pointInTriangle({(float)px, (float)py}, triangle_points, triangle_indices)){
                     colorBuffer[py][px] = GREEN;
                 }
             }
@@ -86,6 +87,7 @@ int main(){
 	}
 	
     UnloadTexture(tex);
+    UnloadImage(img);
 	CloseWindow();
 
     return 0;
